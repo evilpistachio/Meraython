fe6c6f4e4d6a38637c38d15176d3cf7b175ee715
pr0b3r7 <pimentelrobert1@gmail.com>
2020-07-13 14:34:36 -0400

    Adding new code!

--- MeraythonNetworkGetter.py
+++ MeraythonNetworkGetter.py
@@ -4,7 +4,7 @@ import numpy as np
 from meraki_sdk.meraki_sdk_client import MerakiSdkClient
 from meraki_sdk.exceptions.api_exception import APIException
 
-# x_cisco_meraki_api_key = ''
+# x_cisco_meraki_api_key = '1f7c1eab553c0e70ca80c7d3c6168c643131d258'
 # api_key = input("Provide the API key associated with your account: ")
 
 api_key = input("Provide the API key associated with your account: ")
@@ -25,35 +25,25 @@ def getnetworks():
     params = {"organization_id": organization}
     nets = meraki.networks.get_organization_networks(params)
     #    print(type(nets))
-    sorted_nets = pd.DataFrame(nets, columns=['name', 'id', 'productTypes', 'type'])
-    # sorted_nets = sorted(nets, key=lambda x: x['name'])
-    # pprint.pprint(sorted_nets)
-    # sorted_nets.sort_values('name')
-    print(sorted_nets.sort_values('name').to_string())
+    sorted_nets = sorted(nets, key=lambda x: x['name'])
+    pprint.pprint(sorted_nets)
 
 
 def getdevices():
     net_id: str = input("Provide network ID for network whose devices you would like to see:\n " +
                         "Results will be formatted in alphabetic order: ")
     devices = meraki.devices.get_network_devices(net_id)
-
+    pprint.pprint(devices)
     print('Devices will be grouped by name')
-    net_dv = pd.DataFrame(devices, columns=['name', 'firmware', 'serial', 'networkId'])
+    net_dv = pd.DataFrame(devices, columns=['name', 'firmware', 'serial'])
     print(net_dv.to_string())
 
 
 def getswitchports():
     selected_serial: str = input('Input the serial number of the device which switchports you would like to see\n')
     switch_ports = meraki.switch_ports.get_device_switch_ports(selected_serial)
-    sorted_ports = pd.DataFrame(switch_ports)#, columns=['name', 'number', 'type', 'cdp/lldp', 'vlan'])
+    sorted_ports = pd.DataFrame(switch_ports, columns=['name', 'number', 'type', 'vlan'])
     print(sorted_ports.to_string())
-
-
-def get_network_vlans():
-    network_id: str = input("Provide network ID for network device which vlans you'd like to see")
-    vlans_controller = meraki.vlans
-    result = vlans_controller.get_network_vlans(network_id)
-    pprint.pprint(result)
 
 
 def testloopcondition(condition):
@@ -63,8 +53,6 @@ def testloopcondition(condition):
     elif answer in ['n', 'N']:
         condition = False
 
-    # condition = True
-
 
 getorgs()
 getnetworks()
@@ -72,12 +60,7 @@ getdevices()
 
 condition = True
 while condition:
-    selection = int(input('Would you like to see switchports on a particular device? Press 1\n' +
-                          'Or see all vlans within this network? Press 2\n'))
+    selection = int(input('Would you like to see switchports on a particular device? Press 1\n'))
     if selection == 1:
         print('Getting switchports\n')
         getswitchports()
-    elif selection == 2:
-        print('Getting vlans\n')
-    get_network_vlans()
-testloopcondition(condition)
